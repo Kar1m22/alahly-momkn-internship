a35759aecaaa5cea68286946d81363d30397ff01
Kar1m-Ehab <kehab8639@gmail.com>
2026-07-16 05:04:48 +0300

    Done Week 1 - Day 1: Variables Task

---
+++ Week-01/Day-01/src/Main.kt
@@ -0,0 +1,27 @@
+fun main(){
+    val fName = "Karim"     //String
+    val lName = "Ehab"      //String
+    var age = 21            //Int
+    val isEmployee = false  //Boolean
+    val city = "Cairo"      //String
+    val id = 105            //Int
+    var gpa = 2.9           //Double
+    var weight = 75.5f      //Float
+    var height = 180.5f     //Float
+    val gender = "M"        //Char
+
+    println("=======================")
+    println("  Student Information  ")
+    println("=======================")
+
+    println("Full Name      : $fName $lName" )
+    println("Age            : $age years old")
+    println("Gender         : $gender ")
+    println("City           : $city ")
+    println("ID             : $id ")
+    println("Gpa            : $gpa ")
+    println("IsEmployee     : $isEmployee ")
+    println("Weight         : $weight ")
+    println("Height         : $height ")
+
+}
