5982d8f6cce15c96b7b4f304f6a4ba695e2c5647
Kar1m-Ehab <kehab8639@gmail.com>
2026-08-16 11:41:13 +0300

    Done Week 4 - Day 17: ViewModel & StateFlow

---
+++ Week-04/Day-17/app/src/main/java/com/example/day_17/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.example.day_17
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.day_17.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding
+    val viewModel : ContactViewModel by viewModels()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val contactList = viewModel.contacts.value
+        val adapter = ContactAdapter(contactList){ selectedContact ->
+            val intent = Intent(this, DetailActivity::class.java).apply{
+                putExtra("CONTACT_NAME",selectedContact.name)
+                putExtra("CONTACT_PHONE",selectedContact.phone)
+                putExtra("CONTACT_ADDRESS",selectedContact.address)
+
+            }
+            startActivity(intent)
+        }
+        binding.rvContact.adapter = adapter
+        binding.rvContact.layoutManager = LinearLayoutManager(this)
+
+    }
+}
